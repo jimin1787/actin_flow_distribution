2a27952e21974604a8bb573ff150990ae3746b48
jimin1787 <101582861+jimin1787@users.noreply.github.com>
2022-05-16 14:25:38 -0400

    Update frequency_flow_polarplot.m

--- frequency_flow_polarplot.m
+++ frequency_flow_polarplot.m
@@ -38,5 +38,5 @@ saveas(gca,[fileName,'.fig']);
 
 %figure()
 %polarscatter(th,values,'MarkerFaceColor','blue')
-%Thanks to Bhoj Raj Thapa for helping cusmozing MATLAB script
+%Thanks to Bhoj Raj Thapa for helping custmizing MATLAB script
 
